4e3c8110e8a1bc0be72a196d3bb9b2744d3c6edf
AndriyAntonenko <andriyantonenko3.16@gmail.com>
2020-02-09 13:49:04 +0200

    Rename worker.ts to ocr_worker.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Worker } from 'worker_threads';
 const worker = new Worker(path.resolve(__dirname, './worker.import.js'), {
   workerData: {
     value: 15,
-    path: './workers/worker.ts'
+    path: './workers/ocr_worker.ts'
   }
 });
 
