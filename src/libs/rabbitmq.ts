6a4d0499f2416352e1872e60592ffed4c76a06a8
AndriyAntonenko <andriyantonenko3.16@gmail.com>
2020-02-12 02:39:38 +0200

    Add on error listener for rabbitMQ

--- src/libs/rabbitmq.ts
+++ src/libs/rabbitmq.ts
@@ -1,4 +1,10 @@
 import amqplib from 'amqplib';
 import config from 'config';
 
-export const connection = async() => await amqplib.connect(process.env.RABBIT_URL || config.get('rabitMQ.url'));
+export const connection = async() => {
+  const conn: amqplib.Connection = await amqplib.connect(process.env.RABBIT_URL || config.get('rabitMQ.url'));
+  conn.on('error', (err) => {
+    console.log('RabbitMQ error:', err);
+  });
+  return conn;
+};
